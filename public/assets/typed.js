aa0e201f056b9db31f8d3c3a3fdaceff5bfac34e
Rituparna W <rituparnaw16@gmail.com>
2025-01-25 11:27:20 +0100

    init: initial commit

---
+++ public/assets/typed.js
@@ -0,0 +1,15 @@
+new Typed(".typed", {
+  strings: [
+    "a Full Stack Developer.",
+    "a ME(F/A/R/V)N Stack Developer.",
+    "a Backend Developer.",
+    "an Angular Developer.",
+    "a Flutter Developer.",
+    "a React Developer.",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  showCursor: true,
+  cursorChar: "/",
+  loop: true,
+});
